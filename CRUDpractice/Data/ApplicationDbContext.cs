07a926aab06bd77954ad31cc5f23afb99673aaef
Atiqur Rahman <atiqurrahman2312002@gmail.com>
2024-07-08 23:32:31 +0600

    first commit

---
+++ CRUDpractice/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using CRUDpractice.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDpractice.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Student> Students { get; set; } 
+
+    }
+    
+}
